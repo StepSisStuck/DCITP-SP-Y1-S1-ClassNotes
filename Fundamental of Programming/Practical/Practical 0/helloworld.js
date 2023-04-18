da0b487aba3bd5a4d71b126750fd00709421946e
StepSisStuck <stanlylau55@gmail.com>
2023-04-18 15:27:30 +0800

    Update helloworld.js

--- Fundamental of Programming/Practical/Practical 0/helloworld.js
+++ Fundamental of Programming/Practical/Practical 0/helloworld.js
@@ -2,3 +2,7 @@ console.log("Hello World");
 // Make sure you can see your log on the Terminal
 
 // if you have a space, need to add "" 
+// If don't have a space, don't need to add ""
+// Example: File: Hello World.js = node "Hello World.js"
+// Example: File: HelloWorld.js = node HelloWorld.js
+
