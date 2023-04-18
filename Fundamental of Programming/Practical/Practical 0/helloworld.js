e5d7f2089acbe98c976c0537780d59750a2b53f8
StepSisStuck <stanlylau55@gmail.com>
2023-04-18 15:26:25 +0800

    Update

--- Fundamental of Programming/Practical/Practical 0/helloworld.js
+++ Fundamental of Programming/Practical/Practical 0/helloworld.js
@@ -1,2 +1,4 @@
 console.log("Hello World");
 // Make sure you can see your log on the Terminal
+
+// if you have a space, need to add "" 
---
+++ Fundamental of Programming/Practical/Practical 1/The Quick Brown Fox.js
@@ -0,0 +1,2 @@
+console.log("The quick brown fox jumps over the lazy dog");
+// // Make sure you can see your log on the Terminal
