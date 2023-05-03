9f9ea3686b7b1bc527072111764eca1b87508963
StepSisStuck <86126384+StepSisStuck@users.noreply.github.com>
2023-05-03 23:34:06 +0800

    u

---
+++ Fundamental of Programming (FOP)/Practical/Practice/Example1.js
@@ -0,0 +1,14 @@
+let fruits = ['apple', 'banana', 'orange'];
+
+
+// Without indentation
+
+for(let i=0; i<fruits.length; i++){
+console.log(fruits[i]);
+}
+
+// With indentation
+
+for (let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+}
