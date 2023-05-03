39d166858b3e4606e6ef1eba31d907a53c8da40c
StepSisStuck <86126384+StepSisStuck@users.noreply.github.com>
2023-05-04 03:32:59 +0800

    Create highlight.js

---
+++ highlight.js
@@ -0,0 +1,3 @@
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/styles/default.min.css">
+<script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/highlight.min.js"></script>
+<script>hljs.initHighlightingOnLoad();</script>
