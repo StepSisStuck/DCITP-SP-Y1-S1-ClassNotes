dbfdee910ba1a5760bc712a6a08c0b2c1e0aeee6
Stanly Lau <86126384+StepSisStuck@users.noreply.github.com>
2023-05-11 11:14:57 +0800

    Update

---
+++ Fundamental of Programming (FOP)/Practical/Practice/Example6.js
@@ -0,0 +1,29 @@
+// console.log(5 * 20 +7 % 6);
+
+/*
+var a1 = 1, a2 = 3, a3 = 5, a4 = 7;
+var b1 = 7.7;
+
+a2 = ++a1;  
+a1 += a1 + a2;
+console.log(a1);
+
+
+var isWalking = true;
+
+if (isWalking == true) {
+  console.log("Walking");
+} else {
+  console.log("Not walking");
+}
+*/
+
+
+var isWalking = true;
+if (isWalking == true) {
+console.log("Walking");
+}
+else 
+{
+console.log ("Not walking");
+}
